a1aff0af35da9ab426e68d771c13ee1580fa8e5a
Haley Boyle <haleyjboyle@gmail.com>
2019-04-21 09:49:36 -0600

    add apollo to front end

--- app/src/main.js
+++ app/src/main.js
@@ -1,15 +1,37 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import VueApollo from "vue-apollo";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-/* eslint-disable no-new */
+const httpLink = new HttpLink({
+  // You should use an absolute URL here
+  uri: "http://localhost:4000"
+});
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  connectToDevTools: true
+});
+
+Vue.use(VueApollo);
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+  defaultOptions: {
+    $loadingKey: "loading"
+  }
+});
 new Vue({
-  el: '#app',
+  el: "#app",
+  provide: apolloProvider.provide(),
   router,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App)
+});
