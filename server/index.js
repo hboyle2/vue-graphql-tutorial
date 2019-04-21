a8578ba3196c23b55652c0f2c1c28b3db3897bc8
Haley Boyle <haleyjboyle@gmail.com>
2019-04-21 09:32:30 -0600

    basic server

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const { GraphQLServer } = require("graphql-yoga");
+
+const typeDefs = `
+type Query {
+  Wizards: String!
+}
+`;
+
+const resolvers = {
+  Query: {
+    Wizards: () => {
+      return "Wizard List!";
+    }
+  }
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start(() => console.log("Server is running on localhost:4000"));
