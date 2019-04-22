e84e5b6eed4dec801a85b52697f4e5e116547485
Haley Boyle <haleyjboyle@gmail.com>
2019-04-21 21:22:25 -0600

    create wizard

---
+++ app/src/components/CreateWizard.vue
@@ -0,0 +1,39 @@
+
+<template>
+  <div>
+    <div class="flex flex-column mt3">
+      <input v-model="name" type="text" placeholder="name">
+      <input v-model="image" type="text" placeholder="image">
+      <input v-model="house" type="text" placeholder="house">
+    </div>
+    <button @click="createWizard()">Submit</button>
+  </div>
+</template>
+
+
+<script>
+import { CREATE_WIZARD } from "./constants/graphql";
+export default {
+  name: "CreateWizard",
+  data() {
+    return {
+      name: "",
+      image: "",
+      house: ""
+    };
+  },
+  methods: {
+    createWizard() {
+      const { name, image, house } = this.$data;
+      this.$apollo.mutate({
+        mutation: CREATE_WIZARD,
+        variables: {
+          name,
+          image,
+          house
+        }
+      });
+    }
+  }
+};
+</script>
--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import WizardList from "@/components/WizardList";
+import CreateWizard from "../components/CreateWizard.vue";
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: "/",
       name: "WizardList",
       component: WizardList
+    },
+    {
+      path: "/create",
+      name: "CreateWizard",
+      component: CreateWizard
     }
   ]
 });
