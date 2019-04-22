ad08799f44c79b334e5eba3bbe80c7e68144734b
Haley Boyle <haleyjboyle@gmail.com>
2019-04-21 18:09:41 -0600

    added basic frame

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
+import WizardList from "./components/WizardList";
 export default {
-  name: 'App'
-}
+  name: "App",
+  components: {
+    WizardList
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
